cfcc9c16eba497759ce257be3116ba675dc42a4e
samuel-lee-1 <slee232@gmail.com>
2024-07-30 12:19:07 -0400

    yay

---
+++ homework/week 7/problem.py
@@ -0,0 +1,49 @@
+import sys
+import os
+import numpy as np 
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+my_data = pd.read_csv(os.path.join(sys.path[0], "drug200.csv"), delimiter=",")
+my_data[0:5]
+X = my_data[['Age', 'Sex', 'BP', 'Cholesterol', 'Na_to_K']].values
+X[0:5]
+
+from sklearn import preprocessing
+le_sex = preprocessing.LabelEncoder()
+le_sex.fit(['F','M'])
+X[:,1] = le_sex.transform(X[:,1]) 
+
+
+le_BP = preprocessing.LabelEncoder()
+le_BP.fit([ 'LOW', 'NORMAL', 'HIGH'])
+X[:,2] = le_BP.transform(X[:,2])
+
+
+le_Chol = preprocessing.LabelEncoder()
+le_Chol.fit([ 'NORMAL', 'HIGH'])
+X[:,3] = le_Chol.transform(X[:,3]) 
+
+X[0:5]
+y = my_data["Drug"]
+y[0:5]
+
+from sklearn.model_selection import train_test_split
+X_trainset, X_testset, y_trainset, y_testset = train_test_split(X, y, test_size=0.3, random_state=3)
+
+print("X_trainsetX SHAPE:  " + str(X_trainset.shape))
+print("y_trainsetX SHAPE:  " + str(y_trainset.shape))
+print("X_testsetX SHAPE:  " + str(X_testset.shape))
+print("y_testsetX SHAPE:  " + str(y_testset.shape))
+
+drugTree = DecisionTreeClassifier(criterion="entropy", max_depth = 4)
+drugTree # it shows the default parameters
+drugTree.fit(X_trainset,y_trainset)
+predTree = drugTree.predict(X_testset)
+
+print (predTree [0:5])
+print (y_testset [0:5])
+
+from sklearn import metrics
+import matplotlib.pyplot as plt
+print("DecisionTrees's Accuracy: ", metrics.accuracy_score(y_testset, predTree))
